07e54279d56e86b8d3bf7f645255b7d195d6daae
Nathan Little <58357881+Raider140@users.noreply.github.com>
2023-02-22 13:25:26 -0500

    Add files via upload

---
+++ unscramble.py
@@ -0,0 +1,14 @@
+
+'''def choose_random():
+
+def scramble():
+
+def unscramble(): 
+
+def generate_Quiz():'''
+
+def searchText (): 
+    with open('wlist_match8.txt','r') as wordlist:
+        words = wordlist.read()
+        {} = words
+searchText()
